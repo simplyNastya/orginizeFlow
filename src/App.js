dbca77f1ecd26d40f798e98d1e808195bba097fa
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-10-26 08:42:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter basename="/orginizeflow">
+      <BrowserRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />} />
